b1d31cac803674887a11da8b3073f9b9b2c67eeb
Babichev Maxim <maksim.babichev95@gmail.com>
2020-07-30 16:20:26 +0300

    rename analytics.js to cozy.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
-    filename: 'analytics.js',
+    filename: 'cozy.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
